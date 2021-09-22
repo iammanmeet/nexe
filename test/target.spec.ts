07e073559c6be12077067dc852b131f55928de6b
Manmeet Kumar <54315026+iammanmeet@users.noreply.github.com>
2021-09-22 20:33:32 +0530

    console is changed

--- test/target.spec.ts
+++ test/target.spec.ts
@@ -26,6 +26,8 @@ describe('Targets', () => {
   console.log("check issue")
    console.log("check issue")
   console.log("check issue")
+  console.log("check issue")
+  console.log("check issue")
    console.log("check issue")
   it('should stringify and toString', () => {
     expect(JSON.stringify(getTarget(process))).to.equal(`"${getTarget(process)}"`)
